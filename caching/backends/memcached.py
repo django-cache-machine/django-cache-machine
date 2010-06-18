5cb7dc4cade973b99f3648711d5a54c205714dc7
Jeff Balogh <me@jeffbalogh.org>
2010-06-17 17:43:29 -0700

    Django dropped encoding of values for 1.2

--- caching/backends/memcached.py
+++ caching/backends/memcached.py
@@ -6,15 +6,11 @@ from django.utils.encoding import smart_str
 class CacheClass(memcached.CacheClass):
 
     def add(self, key, value, timeout=None):
-        if isinstance(value, unicode):
-            value = value.encode('utf-8')
         if timeout is None:
             timeout = self.default_timeout
         return self._cache.add(smart_str(key), value, timeout)
 
     def set(self, key, value, timeout=None):
-        if isinstance(value, unicode):
-            value = value.encode('utf-8')
         if timeout is None:
             timeout = self.default_timeout
         return self._cache.set(smart_str(key), value, timeout)
