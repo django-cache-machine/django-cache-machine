db2dd762c262080d036e3b24c443ca05ef0c158f
Joao Pedro Francese <joaofrancese@gmail.com>
2015-02-12 17:26:05 -0200

    ManyToMany changes now invalidate the model's cache.
    In addition, invalidation is now always performed on related models as well, traversing ForeignKeys and ManyToManyFields.
    
    Also removes the Redis cache setting in favor of Django's built-in Redis cache backend.
    
    Author:    Joao Pedro Francese <joaofrancese@gmail.com>

--- caching/compat.py
+++ caching/compat.py
@@ -1,6 +1,6 @@
 import django
 
-__all__ = ['DEFAULT_TIMEOUT', 'FOREVER', 'get_cache', 'parse_backend_uri']
+__all__ = ['DEFAULT_TIMEOUT', 'FOREVER', 'get_cache']
 
 
 if django.VERSION[:2] >= (1, 6):
@@ -12,53 +12,21 @@ else:
     FOREVER = 0
 
 
-def get_cache(backend, **kwargs):
-    """
-    Compatibility wrapper for getting Django's cache backend instance
-    """
-    try:
-        from django.core.cache import _create_cache
-    except ImportError:
-        # Django < 1.7
-        from django.core.cache import get_cache as _get_cache
-        from django.core import signals
-        return _get_cache(backend, **kwargs)
+try:
+    from django.core.cache import _create_cache
+    from django.core import signals
 
-    cache = _create_cache(backend, **kwargs)
-    # Some caches -- python-memcached in particular -- need to do a cleanup
-    # at the end of a request cycle. If not implemented in a particular
-    # backend, cache.close() is a no-op
-    signals.request_finished.connect(cache.close)
-    return cache
+    def get_cache(backend, **kwargs):
+        """
+        Compatibility wrapper for getting Django's cache backend instance
+        """
+        cache = _create_cache(backend, **kwargs)
+        # Some caches -- python-memcached in particular -- need to do a cleanup
+        # at the end of a request cycle. If not implemented in a particular
+        # backend, cache.close() is a no-op
+        signals.request_finished.connect(cache.close)
+        return cache
 
-
-def parse_backend_uri(backend_uri):
-    """
-    Converts the "backend_uri" into a host and any extra params that are
-    required for the backend. Returns a (host, params) tuple.
-    """
-    try:
-        from django.core.cache import parse_backend_uri
-        return parse_backend_uri(backend_uri)
-    except ImportError:
-        from django.utils.six.moves.urllib.parse import parse_qsl
-        backend_uri_sliced = backend_uri.split('://')
-        if len(backend_uri_sliced) > 2:
-            raise InvalidCacheBackendError(
-                "Backend URI can't have more than one scheme://")
-        elif len(backend_uri_sliced) == 2:
-            rest = backend_uri_sliced[1]
-        else:
-            rest = backend_uri_sliced[0]
-
-        host = rest
-        qpos = rest.find('?')
-        if qpos != -1:
-            params = dict(parse_qsl(rest[qpos+1:]))
-            host = rest[:qpos]
-        else:
-            params = {}
-        if host.endswith('/'):
-            host = host[:-1]
-
-        return host, params
+except ImportError:
+    # Django < 1.7
+    from django.core.cache import get_cache
