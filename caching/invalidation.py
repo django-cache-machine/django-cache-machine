5a63eeb9a4d831142d0b47e2af04fd41cb1cdc09
Eric Brelsford <ebrelsford@gmail.com>
2015-06-30 20:31:40 -0400

    Use caches rather than get_cache for Django >= 1.7

--- caching/invalidation.py
+++ caching/invalidation.py
@@ -10,7 +10,6 @@ from django.core.cache import cache as default_cache
 from django.core.cache.backends.base import InvalidCacheBackendError
 from django.utils import encoding, translation
 from django.utils.six.moves.urllib.parse import parse_qsl
-print 'hi'
 
 try:
     import redis as redislib
@@ -19,7 +18,7 @@ except ImportError:
 
 # Look for an own cache first before falling back to the default cache
 try:
-    if django.VERSION[:2] >= (1, 8):
+    if django.VERSION[:2] >= (1, 7):
         from django.core.cache import caches
         cache = caches['cache_machine']
     else:
